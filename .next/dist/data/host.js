aa47696169531bdd74dff6d0532524b32d45827d
caozhihui <caozhihui@4399inc.com>
2017-07-29 09:59:10 +0800

    daima

--- .next/dist/data/host.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    SERVER_HOST: 'http://localhost:8081',
-    NEXT_HOST: 'http://localhost:8080'
-};
--- .next/dist/data/url.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var HOST = require('./host');
-var url = {
-    getArticleList: HOST.SERVER_HOST + '/api/article-list',
-    getArticleDetails: HOST.SERVER_HOST + '/api/article-view'
-};
-
-module.exports = url;
